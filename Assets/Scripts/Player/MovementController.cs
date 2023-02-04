c9e192edad187222a36e798afa902fe33486535d
gabol2 <gabrielulate71@gmail.com>
2023-02-03 23:28:18 -0600

    algo

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -16,25 +16,32 @@ namespace freakingpig.controllers
         [SerializeField]
         private float angle;
 
+        [SerializeField]
+        private Animator animator;
+
         // Start is called before the first frame update
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
         // Update is called once per frame
         void Update()
         {
-            if (!Input.anyKey)
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
             {
-                velocity = Vector2.zero;
+                velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+                angle = Mathf.Atan2(Input.GetAxis("Horizontal") * -1, Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
+                rb.SetRotation(angle);
+                rb.velocity = velocity * speed;
+                animator.SetBool("running", true);
             }
             else
             {
-                if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-                {
-                    velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-                    
-                    angle = Mathf.Atan2(Input.GetAxis("Horizontal") * -1, Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
-                    rb.SetRotation(angle);
-                    rb.velocity = velocity * speed;
-                }
+                velocity = Vector2.zero;
+                animator.SetBool("running", false);
             }
+
         }
     }
 
