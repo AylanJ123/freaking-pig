f73f85039285b0cfbaec68b4ad33466c18020487
gabol2 <gabrielulate71@gmail.com>
2023-02-04 09:13:42 -0600

    jet

--- Assets/Scripts/Player/EatingController.cs
+++ Assets/Scripts/Player/EatingController.cs
@@ -8,19 +8,32 @@ namespace freakingpig
     {
         public Collider2D col;
 
+
         // Update is called once per frame
         void Update()
         {
-            if(Input.GetKeyDown(KeyCode.Space)){
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
                 //Layer 3 is plants
-                if(col.IsTouchingLayers(3)){
+                ContactFilter2D cf = new ContactFilter2D();
+                cf.useLayerMask = true;
+                cf.useTriggers = true;
+                cf.layerMask = 1 << 3;
+
+                Collider2D[] cols = new Collider2D[1];
+                col.OverlapCollider(cf, cols);  
+                
+                if (cols.Length >= 1)
+                {
                     Eat();
                 }
             }
         }
 
-        void Eat(){
+        void Eat()
+        {
             //ñam ñam
+            Debug.Log("keikomido");
         }
     }
 }
