c4b796fe77a3b350f16e0d3add0a720a3e5a9dd7
gabol2 <gabrielulate71@gmail.com>
2023-02-03 22:30:43 -0600

    hola

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -2,46 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< Updated upstream
 namespace freakingpig.controllers
 {
     public class MovementController : MonoBehaviour
-=======
-
-public class MovementController : MonoBehaviour
-{
-    public Rigidbody2D rb;
-
-    [SerializeField]
-    private float speed = 10;
-    [SerializeField]
-    private Vector2 velocity = new Vector2();
-
-    // Start is called before the first frame update
-    void Start()
->>>>>>> Stashed changes
     {
+
         public Rigidbody2D rb;
 
-<<<<<<< Updated upstream
+        [SerializeField]
+        private float speed = 10;
+        [SerializeField]
+        private Vector2 velocity = new Vector2();
+
         // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
         // Update is called once per frame
         void Update()
         {
+            if (!Input.anyKey)
+            {
+                velocity = Vector2.zero;
+            }
+            else
+            {
+                velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * 2;
+            }
 
+            rb.velocity = velocity * speed;
         }
-=======
-    // Update is called once per frame
-    void Update()
-    {
-        velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * 2;
-
-        rb.velocity = velocity * speed;
->>>>>>> Stashed changes
     }
+
 }
